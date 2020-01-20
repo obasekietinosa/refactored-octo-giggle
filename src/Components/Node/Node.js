71bcf500115a563f2c6bdcb29d46cbe87ccef233
Etinosa Obaseki <etinosaobaseki@gmail.com>
2020-01-20 09:13:46 +0000

    Node Rendering;
    Add Child nodes recursively;
    Pass methods for adding and deleting nodes as props

--- src/Components/Node/Node.js
+++ src/Components/Node/Node.js
@@ -5,14 +5,25 @@ export default function Node(props) {
     return (
         <div className={ "Node " + (props.position ?? "") }>
             <div className="value">
+                {/* <input onChange={ (e) => props.update(e.target.value) } /> */}
                 {props.node.value}  
+            </div>
+            <div className="text-center">
+                <p>
+                    <button onClick={ () => props.delete(props.node.key) } className="btn btn-danger">Delete Node</button>
+                </p>
+                <p>
+                    <button onClick={ () => props.add(props.node.key, (Math.floor(Math.random() * 100)), "left") } className="btn btn-primary">Add Left Node</button>
+                    &nbsp;
+                    <button onClick={ () => props.add(props.node.key, (Math.floor(Math.random() * 99)), "right") } className="btn btn-primary">Add Right Node</button>
+                </p>
             </div>
             <div className="row">
                 <div className="col-6">
-                    {props.node.left ? <Node node={props.node.left} position="left" /> : ""}
+                    { ! props.node.left ? "" : <Node add={props.add} delete={props.delete} node={props.node.left} position="left" /> }
                 </div>
                 <div className="col-6">
-                    {props.node.right ? <Node node={props.node.right} position="right" /> : ""}
+                    {! props.node.right ? "" : <Node add={props.add} delete={props.delete} update={props.update} node={props.node.right} position="right" /> }
                 </div>
             </div>
         </div>
